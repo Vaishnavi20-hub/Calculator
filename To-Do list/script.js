6a50f446f254b7ec3bdfbbfc1bcab63f76c2f0e8
Vaishanvai Anand Suryawanshi <vaishu5275@gmail.com>
2026-09-08 00:08:27 +0530

    Initial commit

---
+++ To-Do list/script.js
@@ -0,0 +1,35 @@
+function addTask() {
+
+    let input = document.getElementById("taskInput");
+    let taskText = input.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    let span = document.createElement("span");
+    span.textContent = taskText;
+
+    span.onclick = function () {
+        span.classList.toggle("completed");
+    };
+
+    let deleteButton = document.createElement("button");
+
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "delete";
+
+    deleteButton.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
